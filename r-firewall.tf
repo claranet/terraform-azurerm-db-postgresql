59088646f1e400def6763f887676d186c10d6eb2
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2020-02-17 17:17:53 +0100

    AZ-45: re use cidrhost

--- r-firewall.tf
+++ r-firewall.tf
@@ -3,6 +3,6 @@ resource "azurerm_postgresql_firewall_rule" "firewall_rules" {
   name                = each.key
   resource_group_name = var.resource_group_name
   server_name         = azurerm_postgresql_server.postgresql_server.name
-  start_ip_address    = each.value
-  end_ip_address      = each.value
+  start_ip_address    = cidrhost(each.value, 0)
+  end_ip_address      = cidrhost(each.value, -1)
 }
